d7ce505a2c549a0dc33e051829092d98e0be21d0
unknown <matthieu.xillo@hotmail.com>
2019-04-23 16:04:23 +0200

    Changement de l'header

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 
 var app = express();
-app.use(express.static(__dirname+'/public'));
-app.get('/', function(req, res) {
-    res.render('main.ejs');
-});
-app.use(function(req, res, next){
 
-    res.render('404.ejs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.render('main.ejs', {etage: req.params.etagenum});
 });
+
 app.listen(8080);
