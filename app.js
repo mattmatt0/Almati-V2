bcc0c25f847b92a0a89fd7af22004a35b9a5ff84
unknown <matthieu.xillo@hotmail.com>
2019-04-23 17:09:05 +0200

    Changement de la page 404

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-    res.render('main.ejs', {etage: req.params.etagenum});
+    res.render('main.ejs');
 });
+// ... Tout le code de gestion des routes (app.get) se trouve au-dessus
+
+app.use(function(req, res, next){
+    res.render('404.ejs');
+});
+
 
 app.listen(8080);
