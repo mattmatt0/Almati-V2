165788dbee5241c3c7374f2eb5e2d9fe22eca506
Robotechnic <pourjouer.tom@gmail.com>
2019-04-26 17:30:39 +0200

    chemins de l'organisation du site

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require('express'); 
+var cours = require("./routes/cours")
+var user = require("./routes/user")
+var communaute = require("./routes/communaute")
 
 var app = express();
+
 // For cookies
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-
-
-  
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
+
+app.get('', function(req, res) {
     res.render('main.ejs');
-    // res.cookie(C)
 });
+
+app.use("/user",user)
+app.use("/cours",cours)
+app.use("/communaute",communaute)
 
 // If 404:
 app.use(function(req, res, next){
