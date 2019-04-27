4444c82072a630fe48d423e0bd2ac8bae6686e3a
Robotechnic <pourjouer.tom@gmail.com>
2019-04-27 11:20:39 +0200

    url adaptatif selon l'url

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
-var express = require('express'); 
-var cours = require("./routes/cours")
-var user = require("./routes/user")
-var communaute = require("./routes/communaute")
+const express = require('express')
+const cours = require("./routes/cours")
+const user = require("./routes/user")
+const communaute = require("./routes/communaute")
+const ejs = require("ejs")
 
-var app = express();
+const app = express()
+const port = 8080
 
 // For cookies
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
-app.use(express.static(__dirname + '/public'));
+//the view engine
+app.set('view engine', 'ejs');
 
+//add files in public folder (pictures, javascript, css...)
+app.use(express.static(__dirname + '/public'))
+
+// middle ware for get host name and use it in link
+app.use((req,res,next)=>{
+	url = req.url
+	nb = 0
+	urlForLink = "/"
+	if (url != "/favicon.ico" && url != "/")
+	{
+		nb = url.split("/").length-1
+
+		for (var i = 0; i < nb; i++) {
+			urlForLink += "../"
+		}
+	}
+
+	req.urlForLink = urlForLink
+	next()
+})
+
+//main route
 app.get('', function(req, res) {
-    res.render('main.ejs');
+    res.render('main.ejs',{url:req.urlForLink})
 });
 
+//external routes
 app.use("/user",user)
 app.use("/cours",cours)
 app.use("/communaute",communaute)
 
 // If 404:
 app.use(function(req, res, next){
-    res.render('404.ejs');
+    res.render('404.ejs',{url:req.urlForLink})
 });
 
 
-
-app.listen(8080);
+app.listen(8080)
