60e7aa6bf9f1200d8cb34fae14e942869c315824
Robotechnic <pourjouer.tom@gmail.com>
2019-05-04 19:01:30 +0200

    modification menu responsive

--- public/js/header.js
+++ public/js/header.js
@@ -1,17 +1,3 @@
-function colorHeader()
-{
-	// To make the header colorful
-	var divOptions = document.getElementsByClassName("options")[0];
-	var optionsTitles = divOptions.getElementsByTagName("h2");
-	var options = divOptions.getElementsByTagName("a");
-	var len = options.length;
-
-	for(var iterator = 0; iterator < len; ++iterator)
-	{
-		options[iterator].style.filter = "hue-rotate(" + -60 * iterator +"deg)";
-		
-	}
-}
 function hideNews()
 {
 	// For the button
