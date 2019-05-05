4c9d5772fde2d134f0b7a58e2462e41a15204b3d
Robotechnic <pourjouer.tom@gmail.com>
2019-05-05 09:55:08 +0200

    menu responsive entièrement en css + verification pseudo et email dans Sign In

--- public/js/header.js
+++ public/js/header.js
@@ -4,19 +4,3 @@ function hideNews()
 	var newsPannel = document.getElementsByClassName("news")[0] // Where the news are displayed
 	newsPannel.style.display = "none";
 }
-
-function switchMenu()
-{
-	var menu = document.getElementById("menu");
-	switch(menu.style.display)
-	{
-		case "block":
-		menu.style.display = "none";
-		break;
-		default:
-		menu.style.display = "block";
-		break;
-	}
-	
-
-}
--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,11 +1,31 @@
 var pseudoTooltip = document.getElementById("pseudoTooltip")
 var pseudoInput = document.getElementById("pseudo")
 var emailTooltip = document.getElementById("emailTooltip")
+var emailInput = document.getElementById("email")
 var passwordTooltip = document.getElementById("passwordTooltip")
 var passwordConfTolltip = document.getElementById("passwordConfTolltip")
 
+var js = document.getElementById("javascript") //display if the client disable javascript
+js.style.display = 'none'
+
 pseudoChange = () =>{
-	pseudo = pseudoInput.value
-	console.log(pseudo)
+	var pseudo = pseudoInput.value.length
+	if (pseudo<=2 || pseudo > 15)
+		pseudoTooltip.style.display = "block"
+	else
+		pseudoTooltip.style.display = "none"
 }
-pseudoInput.addEventListener("keypress", alert("e"))
+pseudoInput.addEventListener("keyup", pseudoChange)
+pseudoInput.addEventListener("change", pseudoChange)
+
+var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+emailChange = () =>{
+	var email = emailInput.value
+	if (emailRegex.test(email))
+		emailTooltip.style.display = "none"
+	else
+		emailTooltip.style.display = "block"
+}
+emailInput.addEventListener("keyup", emailChange)
+emailInput.addEventListener("change", emailChange)
