8e07e981b0c7774e901e7d3265f15e71fea06977
Robotechnic <robotechnic.developeur@gmail.com>
2020-07-18 19:12:34 +0200

    add mariadb

---
+++ dbShemas.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS Almati CHARACTER SET 'utf8';
+
+
+USE Almati;
+
+#user shema
+CREATE TABLE IF NOT EXISTS users (
+	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	pseudo VARCHAR(15) NOT NULL UNIQUE,
+	image VARCHAR(30) NOT NULL DEFAULT 'default.png',
+	permissions SMALLINT NOT NULL DEFAULT 0
+);
+
+DESCRIBE users;
+SELECT "Commade exécuté avec succés";
